977dca8dfb408cfdd50932503e1634914e200740
snake9byte <snake9byte@tutamail.com>
2024-03-23 20:29:52 +0200

    Add new tests

---
+++ WebContent/controller/Main.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("ZUI5AG.controller.Main", {
+		
+		onInit: function () {
+
+		}	
+	});
+});
